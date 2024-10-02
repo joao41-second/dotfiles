3c04b7c43a2fe7fb13bbdb443463a478e452ce82
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s3.42porto.com>
2024-10-02 11:40:58 +0100

    ja esta

---
+++ nvim_/init.lua
@@ -0,0 +1,6 @@
+dofile('./src/plugins/init.lua')
+dofile('./src/Test_fo_Functionallty/background_change.lua')
+--vim.cmd(":PackerInstall")
+
+
+-- Verifica se o Packer está instalado
---
+++ nvim_/vim-plug/pluggins.vim
@@ -0,0 +1,6 @@
+call plug#begin('~/Desktop/github/dotfiles/nvim/autoload/plugged')
+
+    Plug 'preservim/nerdtree'
+    Plug 'NvChad/NvChad'
+
+call plug#end()

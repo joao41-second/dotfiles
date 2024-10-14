87845b58d7dfe3a39aab84efd3d5833dcb5ed65c
Joao Francisco Perpectuo Figueiredo <jperpect@c2r4s1.42porto.com>
2024-10-14 14:47:17 +0100

    nurmal mode simpele excut

--- nvim/lua/core/macros.lua
+++ nvim/lua/core/macros.lua
@@ -1,1 +1,12 @@
-vim.keymap.set("o", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Mapear Ctrl+n para voltar ao modo Normal
+vim.api.nvim_set_keymap("i", "lk", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "kl", "<Esc>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "hj", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jh", "<Esc>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true, silent = true })
+
+

cba84e9f25e8d56312d33d5d1ade05c7871d8bfd
Joao Francisco Perpectuo Figueiredo <jperpect@c1r13s5.42porto.com>
2024-10-18 13:02:17 +0100

    key shortcut

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("o", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Atalho para gerar documentação de função
+vim.api.nvim_set_keymap("n", "<leader>n", ":Neogen func<CR>", { noremap = true, silent = true })

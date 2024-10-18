be886916a815ca095992103e1c56ca2a147e48f5
Joao Francisco Perpectuo Figueiredo <jperpect@c1r13s5.42porto.com>
2024-10-18 12:59:11 +0100

    add Neogen and cofig

--- nvim/lua/plugins/42header.lua
+++ nvim/lua/plugins/42header.lua
@@ -1,15 +1,20 @@
 return {
     "Diogo-ss/42-header.nvim",
-    cmd = { "header" },
-    jeys = { "<F3>" },
+    cmd = { "Stdheader" },
+    keys = { "<F3>" },
     opts = {
-        default_map = true, -- Default mapping <F1> in normal mode.
-        auto_update = true, -- Update header when saving.
-        user = "jperpct", -- Your user.
-        mail = "jperpect@student.42porto.com", -- Your mail.
-        -- add other options.
+        default_map = true, -- Mapeamento padrão para <F1> no modo normal.
+        auto_update = true, -- Atualizar o header ao salvar.
+        user = "jperpct", -- Seu usuário.
+        mail = "jperpect@student.42porto.com", -- Seu e-mail.
+        -- adicionar outras opções, se necessário.
     },
     config = function(_, opts)
         require("42header").setup(opts)
+
+        -- Criando o comando :Header para fazer o mesmo que :Stdheader
+        vim.api.nvim_create_user_command("STD", function()
+            vim.cmd("Stdheader")
+        end, { nargs = 0 })
     end,
 }

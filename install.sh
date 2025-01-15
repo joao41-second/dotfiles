fe70b5fe18fc0f9e49c77e4a425aa9fd3b0e1040
jperpect <jfigueiredo095@gmail.com>
2025-01-15 09:28:09 +0000

    the update install

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install ncdu
+sudo apt install tmux
+sudo apt install vim
+
+sudo apt install curl
+
+sudo apt install neovim
+sudo apt install make
+sudo apt install gcc
+
+
+

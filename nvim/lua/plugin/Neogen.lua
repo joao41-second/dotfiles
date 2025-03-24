bf70905311cda9ac8182434afe6f1b05c1465788
Joao Francisco Perpectuo Figueiredo <jperpect@c2r5s1.42porto.com>
2025-03-24 12:02:24 +0000

    the new pogin

---
+++ nvim/lua/plugin/Neogen.lua
@@ -0,0 +1,23 @@
+return {
+    "danymat/neogen",
+    config = function()
+        require("neogen").setup({
+            enabled = true, -- Ativa o Neogen automaticamente
+            languages = {
+                c = {
+                    template = {
+                        annotation_convention = "doxygen", -- Usa Doxygen tanto para .c quanto .h
+                    },
+                },
+                cpp = {
+                    template = {
+                        annotation_convention = "doxygen", -- Inclui também para C++
+                    },
+                },
+                -- Se estiver usando outras linguagens como C++, pode adicionar mais aqui.
+            },
+        })
+    end,
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Neogen depende do Treesitter
+    cmd = "Neogen", -- Carrega o plugin somente quando você usa o comando Neogen
+}

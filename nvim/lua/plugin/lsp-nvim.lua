4550adfc5191b200c3ac91ba183db5483a5c8ea5
joao41 <joao41-second@users.noreply.github.com>
2025-10-15 19:43:00 +0100

    remove the norme

--- nvim/lua/plugin/lsp-nvim.lua
+++ nvim/lua/plugin/lsp-nvim.lua
@@ -1,53 +1,90 @@
 
 return {
+  -- LSP
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" }, -- nomes de eventos devem ter letras maiúsculas
     dependencies = {
       { "williamboman/mason.nvim", config = true },
       "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp", -- integração do LSP com nvim-cmp
+      -- não usamos cmp-nvim-lsp para evitar erros em 0.10
     },
     config = function()
       local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      -- Capabilities básicas compatíveis com 0.10
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      -- clangd setup
       lspconfig.clangd.setup({
         capabilities = capabilities,
-        on_attach = function(_, _)
+        on_attach = function(client, bufnr)
+          if type(bufnr) ~= "number" then return end
+
+          -- Diagnostics inline
           vim.diagnostic.config({
-            virtual_text = {
-              prefix = "●", -- ícone antes da mensagem de erro
-              spacing = 2,
-            },
-          })
+            virtual_text = { prefix = "●", spacing = 2 },
+            underline = true,
+            signs = true,
+          }, bufnr)
+
+          -- Keymaps do LSP
+          local opts = { noremap = true, silent = true }
+          vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+          vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+          vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+          vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
         end,
       })
     end,
   },
 
+  -- nvim-cmp (sem LSP para evitar crash no 0.10)
   {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter", -- idem, evento em maiúscula
+    event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip", -- o nome do repositório é case-sensitive
+      "L3MON4D3/LuaSnip",
     },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      -- Carregar snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+
       cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
         mapping = cmp.mapping.preset.insert({
           ["<Tab>"] = cmp.mapping.select_next_item(),
           ["<S-Tab>"] = cmp.mapping.select_prev_item(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
           { name = "buffer" },
           { name = "path" },
+        }),
+      })
+
+      -- Completação no cmdline
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = "buffer" } },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
         }),
       })
     end,

8aea14b965dd5da5c2020baf741d1c4309b0f970
joao41second <jfigueiredo095@gmail.com>
2026-04-24 10:48:06 +0100

    update script the set_confgis the arch

--- arch_config/config_fies/set_confgis.sh
+++ arch_config/config_fies/set_confgis.sh
@@ -1,6 +1,8 @@
+PATHS=$(pwd)
 
-mkdir  ~/.config/hypr/
-ln -s $PATHS/hypr/ ~/.config/hypr/
+echo $PATHS
+rm -fr  ~/.config/hypr/
+ln -s $PATHS/hypr ~/.config/hypr
 
-mkdir  ~/.config/kitty/
-ln -s $PATHS/kitty/ ~/.config/kitty/
+rm -fr  ~/.config/kitty/
+ln -s $PATHS/kitty ~/.config/kitty
